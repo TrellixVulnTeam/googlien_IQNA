cc75be100adaf5fe42f958f88c92bb1a584c8381
Lien <liensilviosanchez@gmail.com>
2021-10-02 15:39:53 -0300

    agregue vista mobile

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
-document.addEventListener('click', e=>{
+/*document.addEventListener('click', e=>{
   const isDropdownButton = e.target.matches("[data-dropdown-button]")
-  
+  if (!isDropdownButton)
+})*/
